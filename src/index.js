c4a1e9bb8bbdb1a14108a36fc939cc30e8250d32
kartikMpatel <kartikpatel7892@gmail.com>
2025-02-16 04:17:06 +0530

    Import Express

--- src/index.js
+++ src/index.js
@@ -6,4 +6,14 @@ import connectDB from "./db/index.js"
 dotenv.config({
     path:"./env"
 })
+
+
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server Is Running On PORT : ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("Connection Failed : ",error)
+})
